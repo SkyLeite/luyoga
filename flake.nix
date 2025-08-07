b6e5fbb16b0ff516a4180a25b6a89e6253719971
Sky Leite <sky@leite.dev>
2025-08-07 09:08:18 -0300

    Add luarocks to devshell

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             luajit
+            luajitPackages.luarocks
             pkg-config
             yoga
           ];
